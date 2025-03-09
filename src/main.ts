8da5c40fe30afbaa307e005fa5832c31494d09a9
SubramanyaRao11 <subramanya11rao@gmail.com>
2025-03-09 15:49:06 +0530

    Added: Simple Swagger Docs

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { RateLimitInterceptor } from './common/interceptors/rate-limit-interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   app.useGlobalInterceptors(new RateLimitInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('GitHub API Integration')
+    .setDescription(
+      'API for fetching GitHub profile and repository information',
+    )
+    .setVersion('1.0')
+    .addTag('github')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
+
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
+
   console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
